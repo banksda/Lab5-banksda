b43ddd2a9d5489ac2194b8daf86a8b576122bdda
Calvin Mlynarczyk <cal.mlynarczyk@gmail.com>
2011-03-27 13:07:07 -0900

    Created TestTwo

--- Lab5/src/PrimeTest.java
+++ Lab5/src/PrimeTest.java
@@ -9,6 +9,20 @@ import org.junit.Test;
 public class PrimeTest {
 	@Test
 	public void testOne() {
-		assertEquals(new ArrayList<Integer>(), PrimeNumbers.generatePrimes(1));
+		assertEquals(list(), PrimeNumbers.generatePrimes(1));
+	}
+	
+	public void testTwo() {
+		assertEquals(list(2), PrimeNumbers.generatePrimes(2));
+	}
+	
+	public ArrayList<Integer> list(int... values) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		for(int index : values) {
+			list.add(index);
+		}
+		
+		return list;
 	}
 }
