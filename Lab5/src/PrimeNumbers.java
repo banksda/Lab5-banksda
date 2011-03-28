adf57075643627032556f0bccfb44fbf51333cc5
Devin Banks <banksda@rose-hulman.edu>
2011-03-28 12:11:50 -0400

    adjusted for testFour

--- Lab5/src/PrimeNumbers.java
+++ Lab5/src/PrimeNumbers.java
@@ -6,11 +6,12 @@ public class PrimeNumbers {
 	public static ArrayList<Integer> generatePrimes(int n) {
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		
-		if(n == 2) {
+		if (n % 2 == 0) {
+			list.addAll(generatePrimes(n/2));
 			list.add(2);
 		}
 		
-		if(n > 2) {
+		if(n % 3 == 0) {
 			list.add(3);
 		}
 		
